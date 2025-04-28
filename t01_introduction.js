2452d69e08a32ab1e4438665414289341d6b7c31
23146ps <23146ps@hvhs.school.nz>
2025-04-28 21:57:59 +0000

    task 01 complete

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,18 @@
-
-
-
+/*
+ t01_introduction.js,
+ javascript introduction,
+ pasha,
+ april 2025
+*/
 console.log("Running t01_introduction.js");
+console.log("Running t01_introduction.js");
+
+//Variables
+
+/*
+Main code
+*/
+
+/*
+Functions
+*/
