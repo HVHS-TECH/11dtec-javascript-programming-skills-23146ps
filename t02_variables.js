3389cdb9aa5be13d9fd8f62a36a0111778804944
23146ps <23146ps@hvhs.school.nz>
2025-04-29 23:38:05 +0000

    variables

---
+++ t02_variables.js
@@ -0,0 +1,18 @@
+/*
+ t02_variables.js,
+ javascript variables,
+ pasha,
+ april 2025
+*/
+console.log("Running t01_introduction.js");
+console.log("Running t01_introduction.js2");
+
+//Variables
+
+/*
+Main code
+*/
+
+/*
+Functions
+*/
