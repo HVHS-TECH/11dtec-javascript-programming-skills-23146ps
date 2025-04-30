6ba5453405719340522409b1a952667d60175cc7
23146ps <23146ps@hvhs.school.nz>
2025-04-30 00:14:56 +0000

    Apr 30, 2025, 12:14 AM

--- t03_maths.js
+++ t03_maths.js
@@ -11,14 +11,14 @@ let userAge = 79;
 let userName = "john";
 let pocketMoney = 60;
 let year = 2025;
-let dead = "dead";
+
 /*
 Main code
 */
 console.log("HI,", userName,);
 console.log("as of", year ,"you are", userAge ,"years old");
 console.log("you were born in", year - userAge,);
-console.log("in 10 years you will be", dead,)
+console.log("in 10 years you will be", userAge + 10,)
 console.log("you have", pocketMoney, "dollars");
 console.log("half of your money is", pocketMoney / 2, );
 /*
