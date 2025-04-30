faf5251e5dfc23f215be633dc1991400cbac9e04
23146ps <23146ps@hvhs.school.nz>
2025-04-30 00:13:06 +0000

    Apr 30, 2025, 12:13 AM

--- t03_maths.js
+++ t03_maths.js
@@ -7,11 +7,20 @@
 console.log("Running t03_maths.js");
 
 //Variables
-
+let userAge = 79;
+let userName = "john";
+let pocketMoney = 60;
+let year = 2025;
+let dead = "dead";
 /*
 Main code
 */
-
+console.log("HI,", userName,);
+console.log("as of", year ,"you are", userAge ,"years old");
+console.log("you were born in", year - userAge,);
+console.log("in 10 years you will be", dead,)
+console.log("you have", pocketMoney, "dollars");
+console.log("half of your money is", pocketMoney - 10, );
 /*
 Functions
 */
