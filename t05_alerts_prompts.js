aef64cc4a09ef3b5a53063304a885d8f931bfbf6
23146ps <23146ps@hvhs.school.nz>
2025-05-01 00:44:25 +0000

    May 1, 2025, 12:44 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -1,17 +1,26 @@
 /*
- t01_introduction.js,
- javascript introduction,
+ t05_alert_prompts.js,
+ javascript maths,
  pasha,
  april 2025
 */
-console.log("Running t05_alerts_prompts.js");
+console.log("Running t05_alerts_prom.js");
 
 //Variables
+let userAge = 79;
+let userName = "john";
+let pocketMoney = 60;
+let year = 2025;
 
 /*
 Main code
 */
-
+console.log("HI,", userName,);
+console.log("as of", year ,"you are", userAge ,"years old");
+console.log("you were born in", year - userAge,);
+console.log("in 10 years you will be", userAge + 10,)
+console.log("you have", pocketMoney, "dollars");
+console.log("half of your money is", pocketMoney / 2, );
 /*
 Functions
 */
