92900d60be3437b4877b026a063320128a41d597
23146ps <23146ps@hvhs.school.nz>
2025-05-01 00:43:00 +0000

    May 1, 2025, 12:43 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,17 @@
+/*
+ t01_introduction.js,
+ javascript introduction,
+ pasha,
+ april 2025
+*/
+console.log("Running t05_alerts_prompts.js");
+
+//Variables
+
+/*
+Main code
+*/
+
+/*
+Functions
+*/
