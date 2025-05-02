23edd2764a3ec9b5f29853b4f0cdd4a430ba7cc1
23146ps <23146ps@hvhs.school.nz>
2025-05-02 02:41:10 +0000

    May 2, 2025, 2:41 AM

---
+++ t08_for_loop.js
@@ -0,0 +1,28 @@
+/*
+ t07_if.js,
+ javascript if statements,
+ pasha,
+ may 2025
+*/
+
+//Variables
+alert("Welcome to the VIRGINITY STEALER 9000™");
+
+let userName = prompt('What is your name?');
+let userAge = prompt('Hi '+ userName +' what is your age?');
+let pocketMoney = prompt('how much money do you have?');
+let year = 2025;
+
+
+/*
+Main code
+*/
+
+if (pocketMoney < "150") {
+    alert("Hi " + userName + " you are poor");
+} else {
+    alert("stfu you nepo baby");
+}
+/*
+Functions
+*/
