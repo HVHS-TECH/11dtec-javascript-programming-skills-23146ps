adb9571c9accf0f7e875829203e8d34ee5ac1815
23146ps <23146ps@hvhs.school.nz>
2025-05-02 02:42:09 +0000

    May 2, 2025, 2:42 AM

--- t08_for_loop.js
+++ t08_for_loop.js
@@ -6,23 +6,13 @@
 */
 
 //Variables
-alert("Welcome to the VIRGINITY STEALER 9000™");
-
-let userName = prompt('What is your name?');
-let userAge = prompt('Hi '+ userName +' what is your age?');
-let pocketMoney = prompt('how much money do you have?');
-let year = 2025;
 
 
 /*
 Main code
 */
 
-if (pocketMoney < "150") {
-    alert("Hi " + userName + " you are poor");
-} else {
-    alert("stfu you nepo baby");
-}
+
 /*
 Functions
 */
