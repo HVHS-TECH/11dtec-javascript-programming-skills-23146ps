91284a39298845475299716cb21aa68e0f693130
23146ps <23146ps@hvhs.school.nz>
2025-05-05 21:59:56 +0000

    funky

---
+++ t09_functions.js
@@ -0,0 +1,28 @@
+/*
+ t09_functions.js,
+ javascript functions,
+ pasha,
+ april 2025
+*/
+console.log("Running t09_functions.js");
+
+//Variables
+function getUserName() {
+    userName = prompt("please input username:");
+}
+function getUserAge() {
+    userAge = prompt("please input age:");
+}
+function getUserMoney() {
+    userMoney = prompt("please input your yearly salary (in $)")
+}
+/*
+Main code
+*/
+getUserName();
+getUserAge();
+getUserMoney();
+alert("your poor lol");
+/*
+Functions
+*/
---
+++ t10_activate_via_button.js
@@ -0,0 +1,17 @@
+/*
+ t10_activate_via_button.js,
+ javascript activation via button,
+ pasha,
+ april 2025
+*/
+console.log("Running t10_activate_via_button.js");
+
+/*
+Main code
+*/
+function start() {
+alert("I virused u lol")
+}
+/*
+Functions
+*/
