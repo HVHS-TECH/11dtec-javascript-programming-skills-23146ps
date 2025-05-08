b715f4bb268c5476ffd6d41265243069eb013060
23146ps <23146ps@hvhs.school.nz>
2025-05-08 01:01:44 +0000

    May 8, 2025, 1:01 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -14,7 +14,7 @@ function getUserAge() {
     userAge = prompt("Hi "+userName+" please input age:");
 }
 function getUserMoney() {
-    userMoney = prompt("Hi "+userName+ " who is "+userAge+" please input your yearly salary (in $)")
+    userMoney = prompt("Hi "+userName+ " who is "+userAge+" years old. please input your yearly salary (in $)")
 }
 /*
 Main code
