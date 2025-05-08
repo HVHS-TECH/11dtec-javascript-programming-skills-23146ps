2de081b3769246205fec9ee79d16c3921ba4b869
23146ps <23146ps@hvhs.school.nz>
2025-05-08 00:50:08 +0000

    May 8, 2025, 12:50 AM

--- t10_activate_via_button.js
+++ t10_activate_via_button.js
@@ -6,11 +6,24 @@
 */
 console.log("Running t10_activate_via_button.js");
 
+//Variables
+function getUserName() {
+    userName = prompt("please input username:");
+}
+function getUserAge() {
+    userAge = prompt("please input age:");
+}
+function getUserMoney() {
+    userMoney = prompt("please input your yearly salary (in $)")
+}
 /*
 Main code
 */
 function start() {
-alert("I virused u lol")
+    getUserName();
+    getUserAge();
+    getUserMoney();
+    alert("your poor lol");
 }
 /*
 Functions
