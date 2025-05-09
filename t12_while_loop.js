9f70d51e747251ebe16706501c2e3e4f0661235b
23146ps <23146ps@hvhs.school.nz>
2025-05-09 03:06:00 +0000

    while

---
+++ t12_while_loop.js
@@ -0,0 +1,21 @@
+/*
+ t12_while_loop.js,
+ javascript while loops,
+ pasha,
+ april 2025
+*/
+console.log("Running t01_while_loop.js");
+
+//Variables
+var password = prompt("do you want the loop to continue?");
+/*
+Main code
+*/
+while (password != "no"){
+    password = prompt("the loop will continue. \ndo you want the loop to continue?");
+}
+
+alert("ok");
+/*
+Functions
+*/
